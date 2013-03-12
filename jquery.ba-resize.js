bd1e257a819bdfbafc9ac3c53a96ee0cd150e5fe
yellis <yaakov@ellisweb.net>
2013-03-12 09:15:07 +0200

    prevent access to uninitialized data object in new_handler()
    fix related to https://github.com/cowboy/jquery-resize/issues/1

--- jquery.ba-resize.js
+++ jquery.ba-resize.js
@@ -190,7 +190,12 @@
       
       function new_handler( e, w, h ) {
         var elem = $(this),
-          data = $.data( this, str_data );
+          data = $.data( this, str_data );    
+                
+        // fix related to https://github.com/cowboy/jquery-resize/issues/1
+        if (data == null) { 
+          data = $.data( this, str_data, { w: null, h: null });
+        }
         
         // If called from the polling loop, w and h will be passed in as
         // arguments. If called manually, via .trigger( 'resize' ) or .resize(),
